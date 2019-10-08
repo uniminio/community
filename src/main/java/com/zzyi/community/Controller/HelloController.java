9b7bfc03380e43e1803ab72fe35269343edc914e
uniminio <1037803024@qq.com>
2019-10-08 19:52:08 +0800

    first commit

---
+++ src/main/java/com/zzyi/community/Controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.zzyi.community.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(@RequestParam(name = "name",required = false)String name, Model model) {
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
